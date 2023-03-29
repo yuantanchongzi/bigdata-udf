0d5442f0840a3fd8bb3d5c300f17c9de65027ca1
yuantanchongzi <864652777@qq.com>
2023-03-29 16:57:17 +0800

    Create JsonArray2HiveData.java

--- src/main/java/cn/wd/udtf/JsonArray2HiveData.java
+++ src/main/java/cn/wd/udtf/JsonArray2HiveData.java
@@ -1,4 +1,4 @@
-package cn.wd.udtf;
+package udtf;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
@@ -18,30 +18,53 @@ import java.util.Iterator;
 
 /**
  * @Author: Francis
- * @Description:
+ * @Description: 将jsonArray字符串中的指定key数据提取出来
+ * select t1.idno,
+ *        detail_list.detail
+ * from (select idno,
+ *              name,
+ *              phone,
+ *              request_time,
+ *              response_time,
+ *              responsecode,
+ *              get_json_object(response_result, '$.var_detail') as var_detail   --这个数据是jsonArray字符串
+ *       from znjf.ods_t_credit_request_log
+ *       where dt = '20230328'
+ *         and request_url = 'http://api.prod.xdecision.net/dataservice/md5/ORModel'
+ *         and idno = '320602197706060032') t1
+ *          lateral view split_json_array(var_detail, 'mobile') detail_list as detail   --这个mobile是key
  * @TIME: Created on 2019/5/9
  * @Modified by:
  */
 public class JsonArray2HiveData extends GenericUDTF {
 
-    private final Logger log = LoggerFactory.getLogger(JsonArray2HiveData.class);
+//    private final Logger log = LoggerFactory.getLogger(JsonArray2HiveData.class);
 
     @Override
     public void process(Object[] args) throws HiveException {
-        String input = args[0].toString();
-
-        int index = args.length;
-
-        JSONArray jsonArray = JSON.parseArray(input);
-        Iterator it = jsonArray.iterator();
-
-        while (it.hasNext()){
+        if (null==args[0]){
+            int index=args.length;
             String[] data_desc = new String[index-1];
-            JSONObject json = (JSONObject) it.next();
-            for (int i =1;i<index;i++){
-                data_desc[i-1]=null!=json.get(args[i].toString())?json.get(args[i].toString()).toString():"";
+            for(int i =1;i<index;i++){
+                data_desc[i-1]="";
             }
             forward(data_desc);
+        }else {
+            String input=args[0].toString();
+
+            int index = args.length;
+
+            JSONArray jsonArray = JSON.parseArray(input);
+            Iterator it = jsonArray.iterator();
+
+            while (it.hasNext()){
+                String[] data_desc = new String[index-1];
+                JSONObject json = (JSONObject) it.next();
+                for (int i =1;i<index;i++){
+                    data_desc[i-1]=null!=json.get(args[i].toString())?json.get(args[i].toString()).toString():"";
+                }
+                forward(data_desc);
+            }
         }
     }
 
